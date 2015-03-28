8e577699e43b6037cca6ef05359e71e4c593a949
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-03-28 11:17:21 +0100

    Create class, setter and getter methods, attribute and assign values accordingly.

--- create_class_from_csv/lib/class_creator.rb
+++ create_class_from_csv/lib/class_creator.rb
@@ -4,7 +4,9 @@ class ClassCreator
 
   def initialize(file_path)
     @file_path = file_path
-    @code = []
+    @csv_data = read_csv_data
+    @new_class = ""
+    @objects = []
   end
   
   def read_csv_data
@@ -15,51 +17,61 @@ class ClassCreator
     File.basename(@file_path, ".*")
   end
 
-  def create
+  def create_class
     file_name = get_file_name
     class_name = file_name.capitalize
-    csv_data = read_csv_data
     if class_name.end_with?("s")
       class_name = class_name[0..-2]
     end
+    klass = Class.new
+    @new_class = Object.const_set class_name, klass
+    create_methods
+    create_objects
+    display_objects
+  end
 
-    klass = Class.new do
-      attr_accessor *csv_data.headers
-      csv_data.headers.each do |header|
+  def create_methods
+    @csv_data.headers.each do |header|
+      @new_class.class_eval do
         define_method header do
           instance_variable_get("@#{header}")
         end
       end
-
-      csv_data.each do |row|
-        row.fields.each do |field|
-          csv_data.headers.each do |header|
-            define_method header do
-              instance_variable_set("@#{header}", field)
-            end
+    end
+    
+    @csv_data.headers.each do |header|
+        @new_class.class_eval do
+          define_method "#{header}=" do |arg|
+            instance_variable_set("@#{header}", arg)
           end
         end
+    end
+  end
+
+
+  def create_objects
+    @csv_data.each do |row|
+      obj = @new_class.new
+      @csv_data.headers.each do |header|
+        obj.send("#{header}=", row[header])
       end
-
-
+      @objects << obj
     end
 
-    new_class = Object.const_set class_name, klass
-    @code << new_class.new
-    p @code
-    p @code[0].name
-    p @code[0].age
-    p @code[0].city
+    @objects
+  end
+
+  def display_objects
+    count = 0
+    @objects.each do |obj|
+      count += 1
+      puts "#{obj.class} #{count}"
+      @csv_data.headers.each do |header|
+        puts "  #{header}: #{obj.send(header)}"
+      end
+    end
   end
 end
 
 cc = ClassCreator.new("persons.csv")
-cc.create
-
-# class Person
-#   def initialize
-    
-#   end
-# end
-
-# obj = [{name: "quava", age: "23", city: "karnal"}, {name: "vilok", age: "23", city: "hissar"}]
+p cc.create_class
