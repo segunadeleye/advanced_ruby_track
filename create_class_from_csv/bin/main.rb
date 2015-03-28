a5243a7a4c6570522b6e8e7c20a3d6cc174312e2
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-03-28 11:46:19 +0100

    Instantiate class in main.rb

--- create_class_from_csv/bin/main.rb
+++ create_class_from_csv/bin/main.rb
@@ -1,24 +1,5 @@
-require "csv"
+require_relative "../lib/class_creator"
 
 file_path = "../lib/persons.csv"
-filename = File.basename(file_path)
-# data = CSV.read(file_path, headers: true, header_converters: :symbol)
-
-CSV.foreach(file_path) do |variable|
-  p variable
-end
-
-# p data.headers.each { |e| p e  }
-# p data[0].fields
-# data.each { |e| p e.fields }
-# data.fields.each { |e| p e }
-# data.each { |e| e.fields.each { |a| p a } }
-# p data[0].fields
-# p data[0][0]
-
-# data.each { |a| p a[:name] }
-
-# p data.headers
-
-# data.headers.each { |header| data.each { |a| p a[header] } }
-
+cc = ClassCreator.new(file_path)
+p cc.create_class_methods_and_objects
