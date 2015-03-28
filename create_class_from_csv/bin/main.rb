5d55a389b088f59e1cf1e80c55389c08c98a4fbf
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-03-28 11:42:14 +0100

    CSV practice and trials.

--- create_class_from_csv/bin/main.rb
+++ create_class_from_csv/bin/main.rb
@@ -2,14 +2,23 @@ require "csv"
 
 file_path = "../lib/persons.csv"
 filename = File.basename(file_path)
-data = CSV.read(file_path, headers: true, header_converters: :symbol)
+# data = CSV.read(file_path, headers: true, header_converters: :symbol)
+
+CSV.foreach(file_path) do |variable|
+  p variable
+end
 
 # p data.headers.each { |e| p e  }
 # p data[0].fields
-data.each { |e| p e.fields }
+# data.each { |e| p e.fields }
 # data.fields.each { |e| p e }
-data.each { |e| e.fields.each { |a| p a } }
+# data.each { |e| e.fields.each { |a| p a } }
 # p data[0].fields
 # p data[0][0]
 
 # data.each { |a| p a[:name] }
+
+# p data.headers
+
+# data.headers.each { |header| data.each { |a| p a[header] } }
+
