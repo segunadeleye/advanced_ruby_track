778cee70ced157c37c720f3b978713b1ce17d0fa
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-04-02 08:22:44 +0100

    Add other methods for creating singleton methods.

--- singleton_method/bin/main.rb
+++ singleton_method/bin/main.rb
@@ -4,8 +4,16 @@ def string.capitalize_consonant
   gsub(/[^aeiou]/) { |match| match.upcase }
 end
 
+class << string
+  def capitalize_vowel
+    gsub(/[aeiou]/) { |match| match.upcase }
+  end
+end
+
 puts string.capitalize_consonant
+puts string.capitalize_vowel
 
 string2 = "I am another instance of the class String."
 
 puts string2.capitalize_consonant
+puts string2.capitalize_vowel
--- singleton_method/lib/string.rb
+++ singleton_method/lib/string.rb
@@ -6,4 +6,18 @@ class String
     end
   end
 
+  def self.underscorify
+    gsub(/\s/, "_")
+  end
+
+end
+
+def String.underscorify
+  gsub(/\s/, "_")
+end
+
+class << String
+  def underscorify
+    gsub(/\s/, "_")
+  end
 end
